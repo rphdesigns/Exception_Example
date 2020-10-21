e4a508d1e64cb29f894ff20b16f2d77223e3514c
Michael Heesch <development@rphdesigns.com>
2020-10-21 08:17:07 -0400

    10/20/2020-Changes

---
+++ Generics_Example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Exception_Example
+{
+    class Generics_Example
+    {
+        public void Generics()
+        {
+            System.Collections.ArrayList list1 = new System.Collections.ArrayList();
+
+            list1.Add(3);
+            list1.Add(5);
+            list1.Add(12);
+            //list1.Add("Hello");
+
+            Console.WriteLine("Non-generic List results...");
+            foreach (int x in list1)
+            {
+                Console.WriteLine(x);
+            }
+
+            List<int> list2 = new List<int>();
+            list2.Add(34);
+            list2.Add(9);
+            list2.Add(21);
+
+            Console.WriteLine("Generic list results...");
+
+            foreach (int item in list2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Exception_Example;
 
 
@@ -9,19 +10,32 @@ namespace Exception_Example
         static void Main(string[] args)
         {
             //CollectionsandArrays.CollectionsandArrays_Method();
+
             //Exception_Example.Exception_Example_Method();
+
             //StringBuilderExample.StringBuilderExample_Method();
+
             //StringReadWrite.StringReadWrite_Runner();
+
             //Using_Properties_and_Fields.Using_Prop_and_Fields();
+
             //Constructor_Example.ConstructorExample();
 
             //Static_Example static_ = new Static_Example();
             //static_.Static_Example_Main();
-            
+
             //Struct_Example struct_Example = new Struct_Example(6,7);
             //struct_Example.DoThis();
 
-            
+            //Generics_Example generics_ = new Generics_Example();
+            //generics_.Generics();
+
+            int num = 2147483647;
+            long bigNum = num;
+
+            int i = 5;
+            double d = 10.5;
+            i = d;
         }
     }
 
